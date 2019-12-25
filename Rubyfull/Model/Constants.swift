0b093db6414b9154fe4476608e283964763a49fe
oimo23 <oimo2338@gmail.com>
2019-12-26 01:25:14 +0900

    定数クラスを作成

--- Rubyfull/Model/Constants.swift
+++ Rubyfull/Model/Constants.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+
+struct Constants {
+    
+    static let shared = Constants()
+
+    let HIRAGANA_API_URL: String
+    let APP_ID: String
+
+    private init() {
+
+        self.HIRAGANA_API_URL = "https://labs.goo.ne.jp/api/hiragana"
+        self.APP_ID = "ce550ffb884060b4c8ab2d76dc5fb47807c571d158f74dadae37d89d873013e7"
+
+    }
+    
+}
