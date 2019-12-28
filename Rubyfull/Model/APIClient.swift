ff82c2dab1aac1c1c5b82adbfef06a2e5fdfd35f
oimo23 <oimo2338@gmail.com>
2019-12-28 10:16:06 +0900

    APIClientを作成

--- Rubyfull/Model/APIClient.swift
+++ Rubyfull/Model/APIClient.swift
@@ -7,3 +7,39 @@
 //
 
 import Foundation
+import Alamofire
+
+enum APIClientError: Swift.Error {
+  case responseError(Swift.Error)
+  case requestError(Swift.Error)
+  case unknownError
+}
+
+final class APIClient {
+  func getHiraganaData(
+    inputtedText: String,
+    completion: @escaping((Swift.Result<TextDataModel, APIClientError>) -> Void)
+  ) {
+    Alamofire.request(
+      Constants.shared.HIRAGANA_API_URL, // リクエスト先のURL
+      method: .post,
+      parameters: [
+        "app_id": Constants.shared.APP_ID,
+        "sentence": inputtedText,
+        "output_type": "hiragana"
+      ]
+    ).responseData { response in
+      switch response.result {
+          case .success(let data):
+            do {
+              let hiraganaText = try JSONDecoder().decode(TextDataModel.self, from: data)
+              completion(.success(hiraganaText))
+            } catch {
+              completion(.failure(.responseError(error)))
+            }
+          case .failure(let error):
+            completion(.failure(.requestError(error)))
+      }
+    }
+  }
+}
